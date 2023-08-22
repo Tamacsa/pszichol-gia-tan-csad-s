7fc0012fe9c633de50b42d1010f22909ef14819b
Tamacsa <121725531+Tamacsa@users.noreply.github.com>
2023-08-22 21:37:03 +0200

    Update index.js
    email elérés

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+document.getElementById("emailButton").addEventListener("click", function() {
+    var recipientEmail = "tamas.eva73@gmail.com";
+    var emailSubject = "Konzultacio!";
+    var emailBody = "Üdvözlöm,\n\n";
+    
+    var mailtoLink = "mailto:" + recipientEmail + "?subject=" + encodeURIComponent(emailSubject) + "&body=" + encodeURIComponent(emailBody);
+    
+    window.open(mailtoLink, "_blank");
+});
